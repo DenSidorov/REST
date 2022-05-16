c9deb2f74353c88ab2257d6c9c1df112eec2d73d
den <alexandrovich07@list.ru>
2022-05-16 21:18:04 +0300

    GET/POST

--- app.js
+++ app.js
@@ -1,16 +1,36 @@
 const express = require('express');
 const app = express();
-const path = require('path')
+const path = require('path');
+const {v4} = require('uuid') // id generator
 
+// БД
+const CONTACTS = [
+    {id: v4(), name: 'Den', value: '8-929-977-92-95', marked: false}
+]
 
+app.use(express.json())
 
+/// GET
+app.get('/api/contacts', (req, res) => {
+    setTimeout(() => { /// Делаем задержку, чтобы увидеть колесо
+        res.status(200).json(CONTACTS)
+    }, 1000)
+    
+})
 
-// Создаём статическую папку
+/// POST
+app.post('/api/contacts', (req, res) =>{
+    console.log(req.body);
+    res.json({test: 1})
+})
+
+/// Следующие запросы должны быть последними:
+// Создаём статическую папку, чтобы index нашёл frontend.js
 app.use(express.static(path.resolve(__dirname, 'client')))
-
+// при любых гет запросах открывает файл:
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'index.html'))
 })
 
-
+// Запускаем сервер
 app.listen(3000, () => console.log('Server has been started on port:3000...'));
--- client/frontend.js
+++ client/frontend.js
@@ -1,7 +1,76 @@
-Vue.createApp({
+import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.esm.browser.js'
+
+Vue.component('loader', {
+    template: `
+        <div style="display: flex;justify-content: center;align-items: center">
+            <div class="text-center">
+                <div class="spinner-border" role="status">
+                <span class="visually-hidden">Loading...</span>
+                </div>
+            </div>
+        </div>
+        `
+})
+
+new Vue({
+    el: '#app',
     data() {
         return {
-            message: 'Hello from Vue!'
+            loading: false,
+            form: {
+                name: '',
+                value: ''
+            },
+            contacts: []
+        }        
+    },
+    computed: {
+        canCreate() {
+            return this.form.value.trim() && this.form.name.trim()
         }
+    },
+    methods: {
+        async createContact() {
+            const {...contact} = this.form;
+            const response = await request('/api/contacts', 'POST', contact);
+            console.log(response);
+            // this.contacts.push({...contact, id: Date.now(), marked: false});
+
+            // this.form.name = this.form.value = '';
+        },
+        markContact(id) {
+            // console.log(id);
+            const contact = this.contacts.find(cont => cont.id === id)
+            contact.marked = true
+            
+        },
+        removeContact(id) {
+            this.contacts = this.contacts.filter(con => con.id !== id)
+        }
+    },
+    async mounted() {
+        this.loading = true
+        this.contacts = await request('/api/contacts')
+        this.loading = false
     }
-}).mount('#app')
+})
+
+async function request(url, method = 'GET', data = null) {
+    try {
+        const headers = {}
+        let body
+
+        if (data) {
+            headers['Content-Type'] = 'application/json'
+            body = JSON.stringify(data)
+        }
+        const response = await fetch(url, {
+            method,
+            headers,
+            body
+        })
+        return await response.json()
+    } catch (e) {
+        console.warn('Error:', e.message)
+    }
+}
